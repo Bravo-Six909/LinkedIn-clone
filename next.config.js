b86d52148658a01641b12110d6a92549efd2bc5a
Ashutosh Mohapatra <ashutoshmohapatra2020@gmail.com>
2022-02-22 22:09:15 +0530

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
+  images: {
+    domains: [
+      "https://media.istockphoto.com/photos",
+      "https://images.pexels.com/photos/"
+    ],
+  }
 }
-
-module.exports = nextConfig
